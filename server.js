d7691b58ddfe8851773729ac68bf8ca119bc53f6
Antonin Grenier <drixion05@gmail.com>
2025-12-19 11:06:31 +0100

    Fixed directory exposition for public final

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import { getAll, getById, create, updateById, deleteById } from "./store.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
